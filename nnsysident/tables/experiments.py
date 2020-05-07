b8a51f7c2a4dafa908c145052c6aeb7f2f8a7e3a
kklurz <kk.lurz@gmail.com>
2020-05-07 18:14:08 +0200

    Add ExperimentsTransfer table

--- nnsysident/tables/experiments.py
+++ nnsysident/tables/experiments.py
@@ -186,7 +186,7 @@ class Experiments(dj.Manual):
     # This table contains the experiments and their descriptions
     experiment_name: varchar(100)                     # name of experiment
     ---
-    -> Fabrikant.proj()
+    -> Fabrikant.proj(experiment_fabrikant='fabrikant_name')
     experiment_comment='': varchar(2000)              # short description 
     experiment_ts=CURRENT_TIMESTAMP:   timestamp      # UTZ timestamp at time of insertion
     """
@@ -202,4 +202,31 @@ class Experiments(dj.Manual):
         experiment_restriction_ts=CURRENT_TIMESTAMP:   timestamp      # UTZ timestamp at time of insertion
         """
 
+@schema
+class ExperimentsTransfer(dj.Manual):
+    # Table to keep track of collections of trained networks that form an experiment.
+    # Instructions:
+    # 1) Make an entry in Experiments with an experiment name and description
+    # 2) Insert all combinations of dataset, model and trainer for this experiment name in Experiments.Restrictions.
+    # 2) Populate the TrainedModel table by restricting it with Experiments.Restrictions and the experiment name.
+    # 3) After training, join this table with TrainedModel and restrict by experiment name to get your results
+    definition = """
+    # This table contains the experiments and their descriptions
+    experiment_name: varchar(100)                     # name of experiment
+    ---
+    -> Fabrikant.proj(experiment_fabrikant='fabrikant_name')
+    experiment_comment='': varchar(2000)              # short description 
+    experiment_ts=CURRENT_TIMESTAMP:   timestamp      # UTZ timestamp at time of insertion
+    """
 
+    class Restrictions(dj.Part):
+        definition = """
+        # This table contains the corresponding hashes to filter out models which form the respective experiment
+        -> master
+        -> Dataset
+        -> Trainer
+        -> Model
+        -> Transfer
+        ---
+        experiment_restriction_ts=CURRENT_TIMESTAMP:   timestamp      # UTZ timestamp at time of insertion
+        """
