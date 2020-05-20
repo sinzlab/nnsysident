7d7d7085eaa9481b8375efc190345856fbebfb5f
kklurz <kk.lurz@gmail.com>
2020-05-20 13:49:03 +0200

    Add deterministic gaussian readout

--- nnsysident/models/models.py
+++ nnsysident/models/models.py
@@ -6,7 +6,7 @@ import copy
 
 from nnfabrik.utility.nn_helpers import get_module_output, set_random_seed, get_dims_for_loader_dict
 from .cores import SE2dCore
-from .readouts import MultipleFullGaussian2d, MultiplePointPooled2d, MultipleSpatialXFeatureLinear
+from .readouts import MultipleFullGaussian2d, MultiplePointPooled2d, MultipleSpatialXFeatureLinear, MultipleDeterministicgaussian2d
 from .utility import unpack_data_info
 
 
@@ -379,6 +379,97 @@ def se2d_spatialxfeaturelinear(dataloaders,
                                             bias=readout_bias,
                                             gamma_readout=gamma_readout,
                                             normalize=normalize
+                                            )
+
+    # initializing readout bias to mean response
+    if readout_bias and data_info is None:
+        for key, value in dataloaders.items():
+            _, targets = next(iter(value))
+            readout[key].bias.data = targets.mean(0)
+
+    model = Encoder(core, readout, elu_offset)
+
+    return model
+
+
+def se2d_deterministicgaussian2d(dataloaders,
+                                    seed,
+                                    elu_offset=0,
+                                    data_info=None,
+                                                                # core args
+                                    hidden_channels=64,
+                                    input_kern=9,
+                                    hidden_kern=7,
+                                    layers=4,
+                                    gamma_input=20.,
+                                    skip=0,
+                                    final_nonlinearity=True,
+                                    momentum=0.9,
+                                    pad_input=False,
+                                    batch_norm=True,
+                                    hidden_dilation=1,
+                                    laplace_padding=None,
+                                    input_regularizer='LaplaceL2norm',
+                                    stack=-1,
+                                    se_reduction=32,
+                                    n_se_blocks=0,
+                                    depth_separable=True,
+                                    linear=False,
+                                                                # readout args,
+                                    readout_bias=True,
+                                    gamma_readout=0.0019,
+
+                                    ):
+    """
+    Model class of a SE2d core and a spatialXfeature (factorized) readout
+    Args:
+    Returns: An initialized model which consists of model.core and model.readout
+    """
+
+    if data_info is not None:
+        n_neurons_dict, in_shapes_dict, input_channels = unpack_data_info(data_info)
+    else:
+        if "train" in dataloaders.keys():
+            dataloaders = dataloaders["train"]
+
+
+        # Obtain the named tuple fields from the first entry of the first dataloader in the dictionary
+        in_name, out_name = next(iter(list(dataloaders.values())[0]))._fields
+
+        session_shape_dict = get_dims_for_loader_dict(dataloaders)
+        n_neurons_dict = {k: v[out_name][1] for k, v in session_shape_dict.items()}
+        in_shapes_dict = {k: v[in_name] for k, v in session_shape_dict.items()}
+        input_channels = [v[in_name][1] for v in session_shape_dict.values()]
+
+    core_input_channels = list(input_channels.values())[0] if isinstance(input_channels, dict) else input_channels[0]
+
+    set_random_seed(seed)
+
+    core = SE2dCore(input_channels=core_input_channels,
+                    hidden_channels=hidden_channels,
+                    input_kern=input_kern,
+                    hidden_kern=hidden_kern,
+                    layers=layers,
+                    gamma_input=gamma_input,
+                    skip=skip,
+                    final_nonlinearity=final_nonlinearity,
+                    bias=False,
+                    momentum=momentum,
+                    pad_input=pad_input,
+                    batch_norm=batch_norm,
+                    hidden_dilation=hidden_dilation,
+                    laplace_padding=laplace_padding,
+                    input_regularizer=input_regularizer,
+                    stack=stack,
+                    se_reduction=se_reduction,
+                    n_se_blocks=n_se_blocks,
+                    depth_separable=depth_separable,
+                    linear=linear)
+
+    readout = MultipleDeterministicgaussian2d(core, in_shape_dict=in_shapes_dict,
+                                            n_neurons_dict=n_neurons_dict,
+                                            bias=readout_bias,
+                                            gamma_readout=gamma_readout,
                                             )
 
     # initializing readout bias to mean response
--- nnsysident/models/readouts.py
+++ nnsysident/models/readouts.py
@@ -1,16 +1,9 @@
 import torch
 
 from torch import nn
-from nnfabrik.utility.nn_helpers import get_io_dims, get_module_output, set_random_seed, get_dims_for_loader_dict
-from collections import OrderedDict, Iterable
-import numpy as np
-import warnings
+from nnfabrik.utility.nn_helpers import get_module_output
 from torch.nn import Parameter
-from torch.nn import functional as F
-from torch.nn import ModuleDict
-from mlutils.constraints import positive
-from mlutils.layers.cores import DepthSeparableConv2d, Core2d, Stacked2dCore
-from mlutils import regularizers
+
 from mlutils.layers.readouts import PointPooled2d, FullGaussian2d, SpatialXFeatureLinear
 
 
@@ -113,3 +106,145 @@ class MultipleSpatialXFeatureLinear(MultiReadout, torch.nn.ModuleDict):
 
     def regularizer(self, data_key):
         return self[data_key].l1(average=False) * self.gamma_readout
+
+
+########################################################################################################################
+
+class DeterministicGaussian2d(nn.Module):
+    """
+    'Gaussian2d' class instantiates an object that can be used to learn a
+    Normal distribution in the core feature space for each neuron, this is
+    applied to the pixel grid to give a simple weighting.
+    The variance should be decreased over training to achieve localization.
+    The readout receives the shape of the core as 'in_shape', the number of units/neurons being predicted as 'outdims', 'bias' specifying whether
+    or not bias term is to be used.
+    The grid parameter contains the normalized locations (x, y coordinates in the core feature space) and is clipped to [-1.1] as it a
+    requirement of the torch.grid_sample function. The feature parameter learns the best linear mapping between the feature
+    map from a given location, and the unit's response with or without an additional elu non-linearity.
+    Args:
+        in_shape (list): shape of the input feature map [channels, width, height]
+        outdims (int): number of output units
+        bias (bool): adds a bias term
+    """
+
+    def __init__(self, in_shape, outdims, bias, **kwargs):
+
+        super().__init__()
+        self.in_shape = in_shape
+        c, w, h = in_shape
+        self.outdims = outdims
+
+        self.mu = Parameter(
+            data=torch.zeros(outdims, 2), requires_grad=True)
+        self.log_var = Parameter(
+            data=torch.zeros(outdims, 2), requires_grad=True)
+        self.grid = torch.nn.Parameter(
+            data=self.make_mask_grid(), requires_grad=False)
+
+        self.features = Parameter(torch.Tensor(1, c, 1, outdims))  # saliency  weights for each channel from core
+
+        if bias:
+            bias = Parameter(torch.Tensor(outdims))
+            self.register_parameter('bias', bias)
+        else:
+            self.register_parameter('bias', None)
+
+        self.initialize()
+
+    def make_mask_grid(self):
+        xx, yy = torch.meshgrid(
+            [torch.linspace(-1, 1, self.in_shape[1]),
+             torch.linspace(-1, 1, self.in_shape[2])])
+        grid = torch.stack([xx, yy], 2)[None, ...]
+        return grid.repeat([self.outdims, 1, 1, 1])
+
+    def mask(self):
+        variances = torch.exp(self.log_var).view(self.outdims, 1, 1, -1)
+        mean = self.mu.view(self.outdims, 1, 1, -1)
+        pdf = self.grid - mean
+        pdf = torch.sum((pdf**2) / variances, dim=-1)
+        pdf = torch.exp(-.5 * pdf)
+        # normalize to sum=1
+        pdf = pdf / torch.sum(pdf, dim=(1, 2), keepdim=True)
+        return pdf
+
+    def initialize(self):
+        """
+        initialize function initializes the mean, sigma for the Gaussian readout and features weights
+        """
+        self.features.data.fill_(1 / self.in_shape[0])
+
+        if self.bias is not None:
+            self.bias.data.fill_(0)
+
+    def feature_l1(self, average=True):
+        """
+        feature_l1 function returns the l1 regularization term either the mean or just the sum of weights
+        Args:
+            average(bool): if True, use mean of weights for regularization
+        """
+        if average:
+            return self.features.abs().mean()
+        else:
+            return self.features.abs().sum()
+
+    def variance_l1(self, average=True):
+        """
+        feature_l1 function returns the l1 regularization term either the
+        mean or just the sum of variances
+        Args:
+            average(bool): if True, use mean of weights for regularization
+        """
+        if average:
+            return self.log_var.abs().mean()
+        else:
+            return self.log_var.abs().sum()
+
+    def forward(self, x, out_idx=None):
+        N, c, w, h = x.size()
+        feat = self.features.view(1, c, self.outdims)
+
+        if out_idx is None:
+            bias = self.bias
+            outdims = self.outdims
+        else:
+            feat = feat[:, :, out_idx]
+            if self.bias is not None:
+                bias = self.bias[out_idx]
+            outdims = len(out_idx)
+
+        mask = self.mask()
+        mask = mask.permute(1, 2, 0)[None, None, ...]
+
+        y = torch.einsum("bchw, jkhwn-> bcn", x, mask)
+        y = (y * feat).sum(1).view(N, outdims)
+
+        if self.bias is not None:
+            y = y + bias
+        return y
+
+    def __repr__(self):
+        c, w, h = self.in_shape
+        r = self.__class__.__name__ + \
+            ' (' + '{} x {} x {}'.format(c, w, h) + ' -> ' + str(self.outdims) + ')'
+        if self.bias is not None:
+            r += ' with bias'
+        for ch in self.children():
+            r += '  -> ' + ch.__repr__() + '\n'
+        return r
+
+
+class MultipleDeterministicgaussian2d(MultiReadout, torch.nn.ModuleDict):
+    def __init__(self, core, in_shape_dict, n_neurons_dict, bias, gamma_readout):
+        # super init to get the _module attribute
+        super(MultipleDeterministicgaussian2d, self).__init__()
+        for k in n_neurons_dict:
+            in_shape = get_module_output(core, in_shape_dict[k])[1:]
+            n_neurons = n_neurons_dict[k]
+
+            self.add_module(k, DeterministicGaussian2d(
+                in_shape,
+                n_neurons,
+                bias=bias)
+                            )
+        self.gamma_readout = gamma_readout
