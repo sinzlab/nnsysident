8b6faaf265c0f30b5ecc1f49495c05b67b286c58
kklur <kk.lurz@gmail.com>
2022-08-18 14:01:48 +0200

    update setup process

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM sinzlab/pytorch:v3.8-torch1.7.0-cuda11.0-dj0.12.7
 RUN pip install --upgrade pip
+RUN pip install --upgrade setuptools
 
 ADD ./data_port /src/data_port
 RUN pip install -e /src/data_port
--- setup.py
+++ setup.py
@@ -1,15 +1,3 @@
-#!/usr/bin/env python
-from setuptools import setup, find_packages
-from os import path
+from setuptools import setup
 
-here = path.abspath(path.dirname(__file__))
-
-setup(
-    name="nnsysident",
-    version="0.0.0",
-    description="Identifying and modelling the biological visual system with deep neural networks",
-    author="Konstantin-Klemens Lurz",
-    author_email="konstantin.lurz@uni-tuebingen.de",
-    packages=find_packages(exclude=[]),
-    install_requires=[],
-)
+setup()
