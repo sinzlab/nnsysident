4c2bafb59b0aca1fe756a5cbea8750f28de3c6ca
kklur <kk.lurz@gmail.com>
2023-07-05 10:11:51 +0200

    Setup for singularity and slurm

--- run.py
+++ run.py
@@ -5,12 +5,12 @@ import datajoint as dj
 import pandas as pd
 import numpy as np
 
-dj.config['database.host'] = os.environ['DJ_HOST']
-dj.config['database.user'] = os.environ['DJ_USERNAME']
-dj.config['database.password'] = os.environ['DJ_PASSWORD']
+dj.config["database.host"] = os.environ["DJ_HOST"]
+dj.config["database.user"] = os.environ["DJ_USERNAME"]
+dj.config["database.password"] = os.environ["DJ_PASSWORD"]
 dj.config["enable_python_native_blobs"] = True
 
-name = 'vei'
+name = "vei"
 os.environ["DJ_SCHEMA_NAME"] = f"metrics_{name}"
 dj.config["nnfabrik.schema_name"] = os.environ["DJ_SCHEMA_NAME"]
 
@@ -28,35 +28,32 @@ from nnsysident.tables.scoring import (
 from nnsysident.utility.data_helpers import extract_data_key
 
 
+# TrainedModel.populate("dataset_hash = '075e942fd72e20d77d672d7a942ebc61'",
+#                       "model_hash = 'acca54db77f06e9f37b72900ddc57263'",
+#                       "trainer_hash = '69601593d387758e9ff6a5bf26dd6739'",
+#                       reserve_jobs=True,)
 
-# unit_ids = [8,  9, 26, 29, 33, 49, 59, 69, 70, 92]
-# ensemble_hash = (TrainedEnsembleModel() & "ensemble_comment = 'gamma models'").fetch1("ensemble_hash")
-#
-# method_hash = (MEIMethod() & "method_comment like '%MEI%'").fetch1("method_hash")
-# MEI().populate(
-#     "unit_id in {}".format(tuple(unit_ids)),
-#     "method_hash = '{}'".format(method_hash),
-#     "ensemble_hash = '{}'".format(ensemble_hash),
-#     display_progress=True,
-# )
-# method_hash = (MEIMethod() & "method_comment like '%CEI%'").fetch1("method_hash")
-# MEI().populate(
-#     "unit_id in {}".format(tuple(unit_ids)),
-#     "method_hash = '{}'".format(method_hash),
-#     "ensemble_hash = '{}'".format(ensemble_hash),
-#     display_progress=True,
-# )
-# method_hash = (MEIMethod() & "method_comment like '%VEI%'").fetch("method_hash")
-# MEI().populate(
-#     "unit_id in {}".format(tuple(unit_ids)),
-#     "method_hash in ('{}', '{}')".format(method_hash[0], method_hash[1]),
-#     "ensemble_hash = '{}'".format(ensemble_hash),
-#     display_progress=True,
-# )
+# TrainedModel.populate("dataset_hash = '775abd59b55c34534c35e24ced244c95'",
+#                       "model_hash = '3c59f65aa3fd05e4a3aae2bb3b081df1'",
+#                       "trainer_hash = '69601593d387758e9ff6a5bf26dd6739'",
+#                       reserve_jobs=True,)
 
 
+ensemble_hash = (TrainedEnsembleModel() & "ensemble_comment = 'gamma models, modulator, shifter'").fetch1(
+    "ensemble_hash"
+)
+unit_ids = [8, 9, 26, 29, 33, 49, 59, 69, 70, 92]
+n_units = len(unit_ids)
 
-
+for mei_type in ["MEI", "CEI", "VEI+, 0.8", "VEI-, 0.8"]:
+    method_hash = (MEIMethod() & f"method_comment like '%{mei_type}%'").fetch1("method_hash")
+    MEI().populate(
+        "unit_id in {}".format(tuple(unit_ids[:n_units])),
+        "method_hash = '{}'".format(method_hash),
+        "ensemble_hash = '{}'".format(ensemble_hash),
+        display_progress=True,
+        reserve_jobs=True,
+    )
 
 
 ### Experiment
@@ -81,76 +78,75 @@ from nnsysident.utility.data_helpers import extract_data_key
 #                                   order="random",)
 
 
-
-######################  Bayesian Search  ###################################
-paths = ["/project/notebooks/data/static20457-5-9-preproc0.zip"]
-img_data_key = extract_data_key(paths[0])
-dataset_fn = "nnsysident.datasets.mouse_loaders.static_loaders"
-dataset_config = {'paths': ['/project/notebooks/data/static20457-5-9-preproc0'],
-                  'batch_size': 64,
-                  'seed': 42,
-                  'loader_outputs': ['images', 'responses'],
-                  'normalize': True,
-                  'exclude': None}
-dataset_config_auto = dict()
-print(dataset_config)
-
-model_fn = "nnsysident.models.models.stacked2d_zig"
-loc = np.exp(-10)
-model_config = {
-    'zero_thresholds': {img_data_key: loc},
-    "init_sigma": 0.4,
-    'init_mu_range': 0.55,
-    'gamma_input': 1.0,
-    'grid_mean_predictor': {'type': 'cortex',
-                            'input_dimensions': 2,
-                            'hidden_layers': 0,
-                            'hidden_features': 0,
-                            'final_tanh': False},
-    "readout_type": "MultipleGeneralizedFullGaussian2d",
-}
-
-print(model_fn)
-print(model_config)
-model_config_auto = dict(
-    feature_reg_weight={"type": "range", "bounds": [1e-2, 1e2], "log_scale": True},
-    hidden_channels={"type": "choice", "values": [32, 64, 128, 256]},
-    layers={"type": "choice", "values": [3, 4, 5, 6]},
-    hidden_kern={"type": "choice", "values": [9, 11, 13, 15, 17]},
-    input_kern={"type": "choice", "values": [9, 11, 13, 15, 17]},
-)
-
-
-trainer_fn = "nnsysident.training.trainers.standard_trainer"
-trainer_config = {'detach_core': False,
-                  'stop_function': 'get_loss',
-                  'maximize': False}
-trainer_config_auto = dict()
-
-autobayes = Bayesian(
-    dataset_fn,
-    dataset_config,
-    dataset_config_auto,
-    model_fn,
-    model_config,
-    model_config_auto,
-    trainer_fn,
-    trainer_config,
-    trainer_config_auto,
-    architect="kklurz",
-    trained_model_table="nnsysident.tables.bayesian.TrainedModelBayesian",
-    total_trials=200,
-)
-
-best_parameters, _, _, _ = autobayes.run()
-
-model_config.update(best_parameters["model"])
-Model().add_entry(
-    model_fn=model_fn,
-    model_config=model_config,
-    model_fabrikant="kklurz",
-    model_comment=f"ZIG, {trainer_config['stop_function']}",
-)
+# ######################  Bayesian Search  ###################################
+# paths = ["/project/notebooks/data/static20457-5-9-preproc0.zip"]
+# img_data_key = extract_data_key(paths[0])
+# dataset_fn = "nnsysident.datasets.mouse_loaders.static_loaders"
+# dataset_config = {'paths': ['/project/notebooks/data/static20457-5-9-preproc0'],
+#                   'batch_size': 64,
+#                   'seed': 42,
+#                   'loader_outputs': ['images', 'responses'],
+#                   'normalize': True,
+#                   'exclude': None}
+# dataset_config_auto = dict()
+# print(dataset_config)
+#
+# model_fn = "nnsysident.models.models.stacked2d_zig"
+# loc = np.exp(-10)
+# model_config = {
+#     'zero_thresholds': {img_data_key: loc},
+#     "init_sigma": 0.4,
+#     'init_mu_range': 0.55,
+#     'gamma_input': 1.0,
+#     'grid_mean_predictor': {'type': 'cortex',
+#                             'input_dimensions': 2,
+#                             'hidden_layers': 0,
+#                             'hidden_features': 0,
+#                             'final_tanh': False},
+#     "readout_type": "MultipleGeneralizedFullGaussian2d",
+# }
+#
+# print(model_fn)
+# print(model_config)
+# model_config_auto = dict(
+#     feature_reg_weight={"type": "range", "bounds": [1e-2, 1e2], "log_scale": True},
+#     hidden_channels={"type": "choice", "values": [32, 64, 128, 256]},
+#     layers={"type": "choice", "values": [3, 4, 5, 6]},
+#     hidden_kern={"type": "choice", "values": [9, 11, 13, 15, 17]},
+#     input_kern={"type": "choice", "values": [9, 11, 13, 15, 17]},
+# )
+#
+#
+# trainer_fn = "nnsysident.training.trainers.standard_trainer"
+# trainer_config = {'detach_core': False,
+#                   'stop_function': 'get_loss',
+#                   'maximize': False}
+# trainer_config_auto = dict()
+#
+# autobayes = Bayesian(
+#     dataset_fn,
+#     dataset_config,
+#     dataset_config_auto,
+#     model_fn,
+#     model_config,
+#     model_config_auto,
+#     trainer_fn,
+#     trainer_config,
+#     trainer_config_auto,
+#     architect="kklurz",
+#     trained_model_table="nnsysident.tables.bayesian.TrainedModelBayesian",
+#     total_trials=200,
+# )
+#
+# best_parameters, _, _, _ = autobayes.run()
+#
+# model_config.update(best_parameters["model"])
+# Model().add_entry(
+#     model_fn=model_fn,
+#     model_config=model_config,
+#     model_fabrikant="kklurz",
+#     model_comment=f"ZIG, {trainer_config['stop_function']}",
+# )
 
 
 ###########################################################################
@@ -160,3 +156,120 @@ Model().add_entry(
 #
 # TestCorr.populate(reserve_jobs=True)
 # TestCorrTransfer.populate(reserve_jobs=True)
+
+
+# ################ MONKEY #################################
+#
+# ######################  Bayesian Search  ###################################
+# dataset_fn = "nnvision.datasets.monkey_loaders.monkey_static_loader"
+# dataset_config = {'dataset': 'CSRF19_V1',
+#                   'neuronal_data_files': [
+#                       '/project/notebooks/data/monkey/CSRF19_V1/neuronal_data/CSRF19_V1_3631896544452.pickle',
+#                       '/project/notebooks/data/monkey/CSRF19_V1/neuronal_data/CSRF19_V1_3632669014376.pickle',
+#                       '/project/notebooks/data/monkey/CSRF19_V1/neuronal_data/CSRF19_V1_3632932714885.pickle',
+#                       '/project/notebooks/data/monkey/CSRF19_V1/neuronal_data/CSRF19_V1_3633364677437.pickle',
+#                       '/project/notebooks/data/monkey/CSRF19_V1/neuronal_data/CSRF19_V1_3634055946316.pickle',
+#                       '/project/notebooks/data/monkey/CSRF19_V1/neuronal_data/CSRF19_V1_3634142311627.pickle',
+#                       '/project/notebooks/data/monkey/CSRF19_V1/neuronal_data/CSRF19_V1_3634658447291.pickle',
+#                       '/project/notebooks/data/monkey/CSRF19_V1/neuronal_data/CSRF19_V1_3634744023164.pickle',
+#                       '/project/notebooks/data/monkey/CSRF19_V1/neuronal_data/CSRF19_V1_3635178040531.pickle',
+#                       '/project/notebooks/data/monkey/CSRF19_V1/neuronal_data/CSRF19_V1_3635949043110.pickle',
+#                       '/project/notebooks/data/monkey/CSRF19_V1/neuronal_data/CSRF19_V1_3636034866307.pickle',
+#                       '/project/notebooks/data/monkey/CSRF19_V1/neuronal_data/CSRF19_V1_3636552742293.pickle',
+#                       '/project/notebooks/data/monkey/CSRF19_V1/neuronal_data/CSRF19_V1_3637161140869.pickle',
+#                       '/project/notebooks/data/monkey/CSRF19_V1/neuronal_data/CSRF19_V1_3637248451650.pickle',
+#                       '/project/notebooks/data/monkey/CSRF19_V1/neuronal_data/CSRF19_V1_3637333931598.pickle',
+#                       '/project/notebooks/data/monkey/CSRF19_V1/neuronal_data/CSRF19_V1_3637760318484.pickle',
+#                       '/project/notebooks/data/monkey/CSRF19_V1/neuronal_data/CSRF19_V1_3637851724731.pickle',
+#                       '/project/notebooks/data/monkey/CSRF19_V1/neuronal_data/CSRF19_V1_3638367026975.pickle',
+#                       '/project/notebooks/data/monkey/CSRF19_V1/neuronal_data/CSRF19_V1_3638456653849.pickle',
+#                       '/project/notebooks/data/monkey/CSRF19_V1/neuronal_data/CSRF19_V1_3638885582960.pickle',
+#                       '/project/notebooks/data/monkey/CSRF19_V1/neuronal_data/CSRF19_V1_3638373332053.pickle',
+#                       '/project/notebooks/data/monkey/CSRF19_V1/neuronal_data/CSRF19_V1_3638541006102.pickle',
+#                       '/project/notebooks/data/monkey/CSRF19_V1/neuronal_data/CSRF19_V1_3638802601378.pickle',
+#                       '/project/notebooks/data/monkey/CSRF19_V1/neuronal_data/CSRF19_V1_3638973674012.pickle',
+#                       '/project/notebooks/data/monkey/CSRF19_V1/neuronal_data/CSRF19_V1_3639060843972.pickle',
+#                       '/project/notebooks/data/monkey/CSRF19_V1/neuronal_data/CSRF19_V1_3639406161189.pickle',
+#                       '/project/notebooks/data/monkey/CSRF19_V1/neuronal_data/CSRF19_V1_3640011636703.pickle',
+#                       '/project/notebooks/data/monkey/CSRF19_V1/neuronal_data/CSRF19_V1_3639664527524.pickle',
+#                       '/project/notebooks/data/monkey/CSRF19_V1/neuronal_data/CSRF19_V1_3639492658943.pickle',
+#                       '/project/notebooks/data/monkey/CSRF19_V1/neuronal_data/CSRF19_V1_3639749909659.pickle',
+#                       '/project/notebooks/data/monkey/CSRF19_V1/neuronal_data/CSRF19_V1_3640095265572.pickle',
+#                       '/project/notebooks/data/monkey/CSRF19_V1/neuronal_data/CSRF19_V1_3631807112901.pickle'],
+#                   'image_cache_path': '/project/notebooks/data/monkey/CSRF19_V1/images/individual',
+#                   'crop': 70,
+#                   'subsample': 1,
+#                   'seed': 1000,
+#                   'time_bins_sum': 12,
+#                   'batch_size': 128}
+# dataset_config_auto = dict()
+#
+# # model_fn = "nnsysident.models.models.stacked2d_gamma"
+# # model_config = {
+# #     "init_sigma": 0.4,
+# #     'init_mu_range': 0.55,
+# #     'gamma_input': 1.0,
+# #     'grid_mean_predictor': None,
+# #     "readout_type": "MultipleGeneralizedFullGaussian2d",
+# # }
+# #
+# # print(model_fn)
+# # print(model_config)
+# # model_config_auto = dict(
+# #     feature_reg_weight={"type": "range", "bounds": [1e-2, 1e2], "log_scale": True},
+# #     hidden_channels={"type": "choice", "values": [32, 64, 128, 256]},
+# #     layers={"type": "choice", "values": [3, 4, 5, 6]},
+# #     hidden_kern={"type": "choice", "values": [9, 11, 13, 15, 17]},
+# #     input_kern={"type": "choice", "values": [9, 11, 13, 15, 17]},
+# #     gamma_input={"type": "range", "bounds": [0.1, 10.]}
+# # )
+#
+#
+# model_fn = "nnsysident.models.models.stacked2d_gamma"
+# model_config = {
+#     "readout_type": "MultipleGeneralizedPointPooled2d",
+#     'hidden_dilation': 2,
+# }
+#
+# print(model_fn)
+# print(model_config)
+# model_config_auto = dict(
+#     gamma_readout={"type": "range", "bounds": [1e-2, 1e2], "log_scale": True},
+#     hidden_channels={"type": "choice", "values": [20, 32, 64, 128]},
+#     layers={"type": "choice", "values": [3, 4, 5, 6]},
+#     hidden_kern={"type": "choice", "values": [7, 9, 11, 13, 15]},
+#     input_kern={"type": "choice", "values": [9, 15, 20, 24, 30]},
+#     gamma_input={"type": "range", "bounds": [0.1, 100.], "log_scale": True}
+# )
+#
+#
+# trainer_fn = "nnsysident.training.trainers.standard_trainer"
+# trainer_config = {'detach_core': False,
+#                   'stop_function': 'get_correlations',
+#                   'maximize': True}
+# trainer_config_auto = dict()
+#
+# autobayes = Bayesian(
+#     dataset_fn,
+#     dataset_config,
+#     dataset_config_auto,
+#     model_fn,
+#     model_config,
+#     model_config_auto,
+#     trainer_fn,
+#     trainer_config,
+#     trainer_config_auto,
+#     architect="kklurz",
+#     trained_model_table="nnsysident.tables.bayesian.TrainedModelBayesian",
+#     total_trials=200,
+# )
+#
+# best_parameters, _, _, _ = autobayes.run()
+#
+# model_config.update(best_parameters["model"])
+# Model().add_entry(
+#     model_fn=model_fn,
+#     model_config=model_config,
+#     model_fabrikant="kklurz",
+#     model_comment=f"Gamma, monkey, {trainer_config['stop_function']}",
+# )
--- slurm_job.py
+++ slurm_job.py
@@ -1,139 +1,144 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
+# import yaml
 import argparse
 import os
 import stat
 import subprocess
 from pathlib import Path
-
-
-def str2bool(v):
-    if isinstance(v, bool):
-        return v
-    if v.lower() in ("yes", "true", "t", "y", "1"):
-        return True
-    elif v.lower() in ("no", "false", "f", "n", "0"):
-        return False
-    else:
-        raise argparse.ArgumentTypeError("Boolean value expected.")
+from time import sleep
 
 
 class SlurmJob:
     def __init__(
-            self,
-            name,
-            time,
-            gpu,
-            num_gpus,
-            memory,
-            email,
-            on_slurm,
-            index=0,
+        self,
+        name,
+        time,
+        gpu,
+        partition,
+        num_gpus,
+        num_cpus,
+        memory,
+        email,
+        unk_args,
+        interactive,
+        cuda_version,
+        exclude_nodes,
+        config_file="",
+        index=0,
     ):
+        host_name = os.environ.get("HOSTNAME", "")
+        self.on_tue_cluster = "bg-slurmb" in host_name
+
         self.name = f"{name}-{index}"
         self.email = email
         self.time = time
 
-        days, hours, minutes = list(
-            map(int, [time.split("-")[0]] + time.split("-")[1].split(":"))
-        )
-        if "2080" in gpu:
-            self.gpu = "gpu-2080ti-dev" if hours < 12 else "gpu-2080ti"
-        else:
-            self.gpu = "gpu-v100"
-        self.num_gpus = num_gpus
+        days, hours, minutes = list(map(int, [time.split("-")[0]] + time.split("-")[1].split(":")))
+        self.gpu = f"A100:{num_gpus}" if gpu == "A100" else num_gpus
+        self.partition = partition
+        self.num_cpus = num_cpus
         self.memory = memory
-        self.on_slurm = on_slurm
+        self.run_args = " ".join(unk_args)
+        self.config_file = config_file
+        # self.cuda_version = "11.7" if gpu == "A100" else "11.2"
+        self.cuda_version = cuda_version
+        self.interactive = interactive
+        self.exclude_nodes = exclude_nodes
+        self.src_dir = "$HOME/projects"
+        self.local_dir = "/local/$USER"
+        self.scratch_emmy_dir = "/scratch-emmy/usr/$USER" if gpu == "A100" else "/scratch/usr/$USER"
+        self.scratch_dir = "/scratch/usr/$USER"
+        self.project_dir_name = "nnsysident"
+        self.singularity_run_command = self.get_singularity_run_command()
 
     @property
     def resource_config_string(self):
-
         if not Path("logs").exists():
             os.mkdir("logs")
-        config_string = """
-#SBATCH --job-name={name}                   # Name of the job
-#SBATCH --ntasks=1                          # Number of tasks
-#SBATCH --cpus-per-task=2                   # Number of CPU cores per task
-#SBATCH --nodes=1                           # Ensure that all cores are on one machine
-#SBATCH --time={time}                       # Runtime in D-HH:MM
-#SBATCH --partition={gpu}                   # Partition to submit to
-#SBATCH --gres=gpu:{num_gpus}               # Number of requested GPUs
-#SBATCH --mem-per-cpu={memory}              # Memory pool for all cores (see also --mem-per-cpu)
-#SBATCH --output=logs/{name}.%j.out         # File to which STDOUT will be written
-#SBATCH --error=logs/{name}.%j.err          # File to which STDERR will be written
-#SBATCH --mail-type=ALL                     # Type of email notification- BEGIN,END,FAIL,ALL
-#SBATCH --mail-user={email}                 # Email to which notifications will be sent
-        """.format(
-            name=self.name,
-            time=self.time,
-            gpu=self.gpu,
-            num_gpus=self.num_gpus,
-            memory=self.memory,
-            email=self.email,
-        )
+        config_string = f"""
+#SBATCH --job-name={self.name}                   # Name of the job
+#SBATCH --ntasks=1                               # Number of tasks
+#SBATCH --cpus-per-task={self.num_cpus}          # Number of CPU cores per task
+#SBATCH --nodes=1                                # Ensure that all cores are on one machine
+#SBATCH --time={self.time}                       # Runtime in D-HH:MM
+#SBATCH --mem-per-cpu={self.memory}              # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH --output=logs/{self.name}.%j.out         # File to which STDOUT will be written
+#SBATCH --error=logs/{self.name}.%j.err          # File to which STDERR will be written
+#SBATCH --mail-type=ALL                          # Type of email notification- BEGIN,END,FAIL,ALL
+#SBATCH --mail-user={self.email}                 # Email to which notifications will be sent
+#SBATCH -p {self.partition}                      # Partition to submit to
+#SBATCH -G {self.gpu}                            # Number of requested GPUs
+#SBATCH --exclude={self.exclude_nodes}           # Exclude nodes
+#SBATCH --constraint=inet                        # Access to internet
+            """
         return config_string
 
-    @property
-    def singularity_run_command(self):
-        cmd_string = """
-singularity run \
---nv \
---env-file .env \
---no-home  \
---bind $SCRATCH:/notebooks/data/,$HOME/projects/:$HOME/projects/,../nnsysident/:/src/nnsysident,../data_port:/src/data_port  \
-singularity.sif  \
-./notebooks/run.py
+    def get_singularity_run_command(self):
+        run_cmd = f"""
+            export SRCDIR={self.src_dir}
+            export JOBDIR={self.local_dir}
+            export JOBOUTDIR={self.scratch_emmy_dir}/outputs/$SLURM_JOB_ID
+            mkdir -p $JOBDIR
+            mkdir -p $JOBOUTDIR
         """
 
-        return cmd_string
-
-    @property
-    def slurm_command(self):
-        cmd_string = """
-scontrol show job $SLURM_JOB_ID  # print some info
+        run_cmd += f"""
+        module load singularity
+        module load cuda/{self.cuda_version}
+        scontrol show job $SLURM_JOB_ID  # print some info
         """
-        return cmd_string
+
+        if self.config_file:
+            run_cmd += f"""
+                cp {self.config_file} $JOBDIR/config.yaml
+            """
+            self.run_args += " --experiment-file $HOME/config.yaml"
+
+        singularity_mode = "instance start" if self.interactive else "exec"
+        instance_name = self.name if self.interactive else ""
+
+        bindings = {self.scratch_dir: "/project/data/", f"$HOME/projects/{self.project_dir_name}": "/project/"}
+        bindings = ",".join([f"{k}:{v}" for k, v in bindings.items()])
+        run_cmd += f""" 
+            singularity {singularity_mode} \
+            --nv \
+            --env-file .env \
+            --no-home  \
+            --bind {bindings}  \
+            singularity.sif {instance_name} \
+            /project/run.py
+            """
+        if self.interactive:
+            run_cmd += "sleep infinity"
+        return run_cmd
 
     def run(self):
+        slurm_job_bash_file = f"./{self.name}.sh"
+        slurm_job_bash_file_content = (
+            "#!/bin/bash \n \n" + self.resource_config_string + "\n" + self.singularity_run_command
+        )
+        with open(slurm_job_bash_file, "w") as f:
+            f.write(slurm_job_bash_file_content)
 
-        if self.on_slurm:
-            slurm_job_bash_file = f"./{self.name}.sh"
-            slurm_job_bash_file_content = (
-                    "#!/bin/bash \n \n"
-                    + self.resource_config_string
-                    + "\n"
-                    + self.slurm_command
-                    + "\n"
-                    + self.singularity_run_command
-            )
+        os.chmod(slurm_job_bash_file, stat.S_IRWXU)
 
-            with open(slurm_job_bash_file, "w") as f:
-                f.write(slurm_job_bash_file_content)
-
-            os.chmod(slurm_job_bash_file, stat.S_IRWXU)
-
-            try:
-                print(
-                    subprocess.check_output("sbatch " + slurm_job_bash_file, shell=True)
-                )
-            finally:
-                # remove the bash file
-                os.remove(slurm_job_bash_file)
-
-        else:
-            print(subprocess.check_output(self.singularity_run_command, shell=True))
+        try:
+            output = subprocess.check_output("sbatch " + slurm_job_bash_file, shell=True)
+            sleep(5)
+            job_id = int(output[20:].strip())
+            node = subprocess.check_output(f"scontrol show job {job_id}| grep ' NodeList'", shell=True).strip()
+            node = str(node).split("=")[1][:-1]
+            print(f"Successfully submitted job with ID {job_id} to node {node}.")
+            # print(self.resource_config_string)
+            # print(self.singularity_run_command)
+        finally:
+            # remove the bash file
+            os.remove(slurm_job_bash_file)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Running jobs on SLURM cluster")
-    parser.add_argument(
-        "--njobs",
-        dest="num_jobs",
-        action="store",
-        default=1,
-        type=int,
-        help="",
-    )
     parser.add_argument(
         "--name",
         dest="name",
@@ -143,10 +148,18 @@ if __name__ == "__main__":
         help="",
     )
     parser.add_argument(
+        "--njobs",
+        dest="num_jobs",
+        action="store",
+        default=1,
+        type=int,
+        help="",
+    )
+    parser.add_argument(
         "--time",
         dest="time",
         action="store",
-        default="0-00:00",
+        default="0-01:00",
         type=str,
         help="time to complete each job. Specify in the following format: D-HH:MM",
     )
@@ -154,8 +167,24 @@ if __name__ == "__main__":
         "--gpu",
         dest="gpu",
         action="store",
-        default="gpu-2080ti",
+        default="A100",
         type=str,
+        help="",
+    )
+    parser.add_argument(
+        "--partition",
+        dest="partition",
+        action="store",
+        default="grete:shared",
+        type=str,
+        help="",
+    )
+    parser.add_argument(
+        "--ncpus",
+        dest="num_cpus",
+        action="store",
+        default=4,
+        type=int,
         help="",
     )
     parser.add_argument(
@@ -170,14 +199,8 @@ if __name__ == "__main__":
         "--memory",
         dest="memory",
         action="store",
-        default=3000,
-        type=int,
-        help="",
-    )
-    parser.add_argument(
-        "--force-rebuild",
-        dest="force_rebuild",
-        action="store_true",
+        default="3G",
+        type=str,
         help="",
     )
     parser.add_argument(
@@ -189,29 +212,38 @@ if __name__ == "__main__":
         help="",
     )
     parser.add_argument(
-        "--slurm",
-        dest="slurm",
-        default=True,
-        type=str2bool,
-        help="Specify whether this is a job on SLURM or a normal GPU server.",
+        "--cuda",
+        dest="cuda_version",
+        action="store",
+        default="11.7",
+        type=str,
+        help="Cuda version to use",
     )
-
-    args = parser.parse_args()
-    if not Path("./singularity.sif").exists() or args.force_rebuild:
-        subprocess.check_output(
-            "singularity build --force --fakeroot singularity.sif ./singularity.def",
-            shell=True,
-        )
+    parser.add_argument(
+        "--exclude",
+        dest="exclude_nodes",
+        action="store",
+        default="",
+        type=str,
+        help="List of nodes to exclude",
+    )
+    parser.add_argument("--interactive", dest="interactive", action="store_true", default=False, help="")
+    args, unk_args = parser.parse_known_args()
 
     for job_index in range(args.num_jobs):
         job = SlurmJob(
-            args.name,
-            args.time,
-            args.gpu,
-            args.num_gpus,
-            args.memory,
-            args.email,
-            args.slurm,
+            name=args.name,
+            time=args.time,
+            gpu=args.gpu,
+            partition=args.partition,
+            num_gpus=args.num_gpus,
+            num_cpus=args.num_cpus,
+            memory=args.memory,
+            email=args.email,
+            unk_args=unk_args,
+            interactive=args.interactive,
+            cuda_version=args.cuda_version,
+            exclude_nodes=args.exclude_nodes,
             index=job_index,
         )
         job.run()
